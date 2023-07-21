1a791e16ad4fe7aaa910e569ebe6270b127f8d40
CrossDev <joancruz0502@hotmail.com>
2023-07-20 23:45:38 -0400

    add contact Section and Footer

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
+// const btn = document.getElementById("contavar
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault()
+    
+// })
+
 $(document).ready( () => {
     
     // scroll and navbar script
@@ -16,4 +22,34 @@ $(document).ready( () => {
         $('.home .max-width').toggleClass("disable");
         $('.menu-btn i').toggleClass("active")
     });
+
+    $('.submit').on('click', () => {
+
+        var name = $('#name').val();
+        var subject = $('#subject').val();
+        var email = $('#email').val();
+        var message = $('#message').val();
+        var body = `<h4>${name}</h4><br/> <h5>Email: ${email} - Subject: ${subject}</h5> <br/> <p>${message}</p>`
+        if(name != ''){
+            Email.send({
+                SecureToken : "18cfce31-d3b3-4deb-b243-028aec3c179b",
+                To : `joancruz0502@hotmail.com`,
+                From : `joancruz0502@gmail.com`,
+                Subject : `Contact Message`,
+                Body : body
+            }).then(
+            message => alert(message)
+            );
+        }
+        
+        
+    })
+
+
 })
+
+
+
+
+
+//f54da20d-daff-4e9b-af69-194960e3b519 hotmail email token
